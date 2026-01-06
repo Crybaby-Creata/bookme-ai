07c0a46fc25ed240edf8fbfb2d8c61f545eec80b
Crybaby-Creata <khumomothobi18@gmail.com>
2026-01-06 16:12:55 +0200

    Add load_styles function with CSS styles

---
+++ styles.py
@@ -0,0 +1,30 @@
+def load_styles():
+    return """
+    <style>
+    body {
+        background-color: #0b0b0b;
+        color: #f5c400;
+    }
+
+    .block {
+        background-color: #151515;
+        padding: 15px;
+        border-left: 6px solid #f5c400;
+        margin-bottom: 15px;
+    }
+
+    .stButton button {
+        background-color: #f5c400;
+        color: black;
+        font-weight: bold;
+        border-radius: 6px;
+        padding: 8px 14px;
+    }
+
+    .stTextArea textarea {
+        background-color: #1c1c1c;
+        color: white;
+    }
+    </style>
+    """
+
