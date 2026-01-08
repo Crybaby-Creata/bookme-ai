626acf76e634e3137be9450aecef08e9052e0323
Crybaby-Creata <khumomothobi18@gmail.com>
2026-01-08 19:37:59 +0200

    Refactor styles for button and textarea

--- styles.py
+++ styles.py
@@ -5,26 +5,23 @@ def load_styles():
         background-color: #0b0b0b;
         color: #f5c400;
     }
-
     .block {
         background-color: #151515;
         padding: 15px;
         border-left: 6px solid #f5c400;
         margin-bottom: 15px;
     }
-
     .stButton button {
         background-color: #f5c400;
         color: black;
         font-weight: bold;
         border-radius: 6px;
-        padding: 8px 14px;
     }
-
-    .stTextArea textarea {
-        background-color: #1c1c1c;
-        color: white;
+    textarea {
+        background-color: #1c1c1c !important;
+        color: white !important;
     }
     </style>
     """
+
 
